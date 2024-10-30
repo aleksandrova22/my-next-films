f06e81209cd97391fe0f6dd57ee51416d703e5d6
aleksandrova22 <lialia1986@mail.ru>
2024-10-30 21:20:01 +0700

    ver3

--- components/Films/Film.jsx
+++ components/Films/Film.jsx
@@ -1,81 +1,87 @@
 import { Fetcher } from "@/components/Fetcher";
 import { useCallback, useState } from "react";
+import { PopupWindow } from "../PopupWindow";
+import classes from './Film.module.css';
 // import { ObjTable } from "@/components/ObjTable";
 // import { config } from "@/components/configs/jsph";
 
 
-export default  function GetFilms() {
+export default function GetFilms() {
     const
         [films, setFilms] = useState(''),
         [value, setValue] = useState('red'),
         [status, setStatus] = useState(false),
-      memoizedCallback = useCallback(
-            () =>  setStatus(true),
+        memoizedCallback = useCallback(
+            () => setStatus(true),
             [],
-          ),
+        ),
         indexPage = 1,
         onload = useCallback(data => setFilms(data), []);
-    // useEffect(() => {
-    //     async function fetchFilms() {
-    //         try {
-    //             setError(null);
-    //             const
-    //                 indexPage = 1,
-    //                 url = new URL('http://www.omdbapi.com/?apikey=f7517aad&s=' + value + '&page=' + indexPage),
-    //                 res = await fetch(url);
-
-    //             if (!res.ok) throw (new Error(res.status));
-    //             const
-    //                 u = await res.json();
-    //             setFilms(u);
-
-    //         } catch (err) {
-    //             console.log('catch', err);
-    //             setError(err);
-    //         }
-    //     };
-    //     fetchFilms();
-
-    // }, [value]);
-
-    // if (error)
-    //     return <div className={classes.error}>Error: {error.toString()}</div>;
-    // if (!films)
-    //     return <div className={classes.spinner}>loading...</div>;
-
 
     return <>
-        <input value={value} onInput={event => setValue(event.currentTarget.value)} />
-        <button  onClick={memoizedCallback}> Click me </button>
+        <input type='search' value={value} onInput={event => setValue(event.currentTarget.value)} />
+        <button onClick={memoizedCallback}> Click me </button>
         {console.log(films)}
-       { (status) &&
-        <Fetcher
-            url={'http://www.omdbapi.com/?apikey=f7517aad&s=' + value + '&page=' + indexPage}
-            onLoad={onload}>
-            <Films data={films.Search} />
-        </Fetcher>
-       
-       
+        {(status) &&
+            <Fetcher
+                url={'http://www.omdbapi.com/?apikey=f7517aad&s=' + value + '&page=' + indexPage}
+                onLoad={onload}>
+                <Films data={films.Search} />
+            </Fetcher>
+
         }
     </>
 }
 
-
 function Films({ data }) {
     if (!data) return <span>no found</span>
-    return <div>
+    return <div className={classes.film}>
         {console.log(data)}
-                {data.map(f => <Film film={f} key={f.id}/>)}
+        {data.map(f => <Film film={f} key={f.imdbID} />)}
     </div>;
 }
 
-
 function Film({ film }) {
-    return <fieldset>
+    const
+    [visible, setVisible] = useState(false);
+
+    return <fieldset  onClick={() => setVisible(true)}>
         <legend> film: {film.imdbID}</legend>
         <span > {film.Title} </span>
         <span>{film.Year}</span>
         <span>{film.Type}</span>
         <img src={film.Poster} />
+
+
+        {visible && <PopupWindow >
+            <button onClick={() => setVisible(false)}>close</button>
+            {/* <div onClick={() => setVisible(false)}> */}
+            <img src={'http://img.omdbapi.com/?apikey=f7517aad&i=' + film.imdbID} />
+            {/* </div> */}
+            {/* <Fetcher
+                url={'http://img.omdbapi.com/?apikey=f7517aad&i=' + film.imdbID}
+                onLoad={onload}>
+                                    
+            </Fetcher> */}
+
+        </PopupWindow>}
+     
     </fieldset>;
 }
+
+
+//подробности о фильме по клику на картинку
+
+
+//вывод в модальное окно
+
+function FilmModal() {
+    const
+        [visible, setVisible] = useState(false);
+    return <fieldset>
+        <button onClick={() => setVisible(true)}>open</button>
+        
+    </fieldset>
+}
+
+
---
+++ components/PopupWindow/index.jsx
@@ -0,0 +1,5 @@
+import classes from './index.module.css';
+
+export function PopupWindow({children}){
+return <div className={classes.popup}>{children}</div>
+}
